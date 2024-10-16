81244d71fb0f008a2a205dd0a02e4389d0c27613
Shafira Andaya Putri <92356292+shafiraandayaputri@users.noreply.github.com>
2024-10-16 22:12:25 +0700

    Add files via upload

---
+++ Website-Personal-main/js/custom.js
@@ -0,0 +1,45 @@
+
+  (function ($) {
+  
+  "use strict";
+
+    // NAVBAR
+    $('.navbar-collapse a').on('click',function(){
+      $(".navbar-collapse").collapse('hide');
+    });
+
+    $(function() {
+      $('.hero-slides').vegas({
+          slides: [
+              { src: 'images/slides/SAVE_20210425_113357.jpg' },
+              { src: 'images/slides/Snapinsta.app_285193790_3137846649803167_6653088797292293374_n_1080.jpg' },
+              { src: 'images/slides/Snapinsta.app_440856458_18225829507282863_7241376434212987861_n_1080.jpg' }
+          ],
+          timer: false,
+          animation: 'kenburns',
+      });
+    });
+    
+    // CUSTOM LINK
+    $('.smoothscroll').click(function(){
+      var el = $(this).attr('href');
+      var elWrapped = $(el);
+      var header_height = $('.navbar').height() + 60;
+  
+      scrollToDiv(elWrapped,header_height);
+      return false;
+  
+      function scrollToDiv(element,navheight){
+        var offset = element.offset();
+        var offsetTop = offset.top;
+        var totalScroll = offsetTop-navheight;
+  
+        $('body,html').animate({
+        scrollTop: totalScroll
+        }, 300);
+      }
+    });
+  
+  })(window.jQuery);
+
+
